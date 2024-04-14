3867425794802060cc833fcccb861aa9d9cfe5bd
HASHEM-S <popleeswager077@gmail.com>
2024-04-14 09:31:27 -0700

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,41 +1,14 @@
 #!/usr/bin/python3
-
+"""Module that lists all states from the hbtn_0e_0_usa database."""
 import sys
 import MySQLdb
 
-def list_states_with_N(username, password, database):
-    # Connect to MySQL server
-    try:
-        connection = MySQLdb.connect(host="localhost",
-                                     user=username,
-                                     passwd=password,
-                                     db=database,
-                                     port=3306)
-        cursor = connection.cursor()
-
-        # Execute SQL query
-        query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-        cursor.execute(query)
-
-        # Fetch and display results
-        results = cursor.fetchall()
-        for row in results:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("Error connecting to MySQL database:", e)
-
-    finally:
-        if connection:
-            connection.close()
-
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-    
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    # Get MySQL credentials from command-line arguments
+    # Connect to MySQL server
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
 
-    list_states_with_N(username, password, database)
+    # Execute the SQL query to retrieve all states sorted by id
+    c.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in c.fetchall() if state[1][0] == "N"]
